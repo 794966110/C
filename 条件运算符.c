5e1e8ffc8a5c480194bbc2e4afe6893f3fccbccd
794966110 <55945069+794966110@users.noreply.github.com>
2019-10-21 20:18:24 +0800

    Update 条件运算符.c

--- 条件运算符.c
+++ 条件运算符.c
@@ -8,3 +8,16 @@ void main()
 	scanf("%d%d",&a,&b);
 	printf("max=%d",a>b?a:b);
 } 
+
+
+
+#include<stdio.h>
+
+void main()
+
+{
+	char ch;
+	scanf("%c",&ch);
+	ch=(ch>='A'&&ch<='Z')?(ch+32):ch;
+	printf("%c\n",ch);
+} 
