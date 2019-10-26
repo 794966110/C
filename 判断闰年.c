fe145763a77391894a6db7ed940f182023e4e77a
794966110 <55945069+794966110@users.noreply.github.com>
2019-10-26 20:18:27 +0800

    Update 判断闰年.c

--- 判断闰年.c
+++ 判断闰年.c
@@ -19,3 +19,46 @@ void main()
 		printf("%d不是闰年\n");
 	}
 }
+
+
+
+
+
+
+#include<stdio.h>
+
+void main()
+
+{
+	int year,leap;
+	scanf("%d",&year);
+	if (year%4==0)
+	{
+		if(year%100==0)
+		{
+			leap=0;
+
+		}
+		else
+		{
+			leap=1;
+		}
+	}
+	
+	else 
+	{
+		leap=0;
+	}
+	if(1==leap)
+	{
+		printf("%d is",year);
+	}
+	else
+	{
+		printf("%d is not",year);
+	}
+	
+	printf(" a leap year\n");
+
+
+}
