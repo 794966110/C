89b4682727c729d09aa90a7ae97a31e48458a4ca
794966110 <55945069+794966110@users.noreply.github.com>
2019-10-30 12:38:26 +0800

    Create 邮费计算.c

---
+++ 邮费计算.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+int main(void)
+
+{
+	int c,s;
+	float p,w,f,d;
+	scanf("%d,%f,%f",&s,&w,&p);
+	if(s>=3000)
+	{
+		c=12;
+	}
+	else
+	{
+		c=s/250;
+	}
+	   switch(c)
+	{
+	case 0:d=0;break;
+	case 1:d=2;break;
+	case 2:
+	case 3:d=5;break;
+	case 4:
+	case 5:
+	case 6:
+	case 7:d=8;break;
+	case 8:
+	case 9:
+	case 10:
+	case 11:d=10;break;
+	case 12:d=15;break;
+
+	}
+
+	f=p*s*w*(1-d/100);
+	printf("%f\n",f);
+
+	return 0;
+}
