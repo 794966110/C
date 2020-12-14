427101d08282c77ca90f206ca6182e58cf665022
794966110 <55945069+794966110@users.noreply.github.com>
2020-12-14 22:17:10 +0800

    Update main.c

--- 二叉链/main.c
+++ 二叉链/main.c
@@ -14,7 +14,9 @@ int main()
 	printf("先序遍历（非递归数组实现）：");preorder2(b);putchar('\n');
 	printf("先序遍历（非递归栈实现）：");preorder3(b);putchar('\n');
 	printf("中序遍历：");inorder(b);putchar('\n');
-	printf("中序遍历：");postorder(b);putchar('\n');
+	printf("中序遍历（非递归）：");inorder2(b);putchar('\n');
+	printf("后序遍历：");postorder(b);putchar('\n');
+	//printf("后序遍历：");postorder2(b);putchar('\n');
 	printf("所有结点个数为：%d\n", allnodes(b));
 	printf("叶子结点数：");
 	printf("%d\n", leavesnodes(b));
@@ -24,8 +26,7 @@ int main()
 	printf("第%d层有%d个结点\n",k, n);
 	printf("A的两个子树是否相同：%d\n", similar(b->lchild, b->rchild));
 	printf("G的祖先结点为：");ancestor(b, 'G');putchar('\n');
-
-
+	
 	destory(&b);
 	return 0;
 }
